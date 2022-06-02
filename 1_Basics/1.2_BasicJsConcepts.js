4844da85ca8971816e683276f4033288ce55dfd4
gauravthorath <gauravjobs25@gmail.com>
2022-06-02 12:09:31 +0530

    added comparisons and math operations file names are updated

---
+++ 1_Basics/1.2_BasicJsConcepts.js
@@ -0,0 +1,5 @@
+let js = "amazing";
+if (js === "amazing") console.log("Javascript is fun");
+js = "Boring";
+if (js !== "amazing") console.log("Javascript is boring");
+console.log(40 + 8 + 23 - 10);
