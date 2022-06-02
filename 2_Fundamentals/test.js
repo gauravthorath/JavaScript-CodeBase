0e7f593c9c7b0a7cc9e839ab5d0c9f93efacd561
gauravthorath <gauravjobs25@gmail.com>
2022-06-02 16:52:59 +0530

    solved tests on values and variabeles

---
+++ 2_Fundamentals/test.js
@@ -0,0 +1,10 @@
+// Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "India";
+let continent = "Asia";
+let population = "1.5 billions";
+
+console.log(country, continent, population);
